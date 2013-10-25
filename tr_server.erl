872f740e462a0e30648d88c597150639230fd86b
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2013-10-25 15:56:21 +0200

    temporarily comment out unused export

--- tr_server.erl
+++ tr_server.erl
@@ -20,8 +20,8 @@
         ]).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2]).%,
+%         terminate/2, code_change/3]).
 
 %% Macros
 -define(SERVER, ?MODULE).
